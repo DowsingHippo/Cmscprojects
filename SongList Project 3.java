e78eea38c1b3fa6f1aace603e6e637c213df1c1f
Logan Miller <46401738+DowsingHippo@users.noreply.github.com>
2024-09-29 16:47:27 -0400

    Update SongList Project 3.java

--- SongList Project 3.java
+++ SongList Project 3.java
@@ -1,3 +1,30 @@
+/*
+For this project, you will be searching and sorting data related to bridges.data_src_dependent.Song objects.
+The Song data will be obtained from an online database and accessed via the BRIDGES API - https://bridgesdata.herokuapp.com/api/datasets/songLinks to an external site..
+
+Read song data from the BRIDGES API.
+Create a custom linked list of SLelement objects (http://bridgesuncc.github.io/doc/java-api/current/html/classbridges_1_1base_1_1_s_lelement.htmlLinks to an external site.) and populate the linked list with
+Song objects created from the dataset.
+
+To simplify the implementation of the linked list, you are required to implement this interface List.java Download List.java 
+
+The class heading for the SongList class must be 
+public class SongList implements cmsc256.List<bridges.data_src_dependent.Song>
+
+Provide an additional method that returns a formatted list of all the songs by an artist (name given as a String parameter) to the method that appear on the linked list, grouped by album title 
+(in alphabetical order) and by song title (in alphabetical order).
+
+The name of the method must be:
+ public String getSongsByArtist(String artist)
+ 
+The returned String is to be formatted with each song on a separate line with the song title, artist and album labeled as shown here:
+Title: Harder, Better, Faster, Stronger Artist: Daft Punk Album: Discovery
+
+If no songs by the given artist are on the playlist, an appropriate message is to be displayed. Such as, “There are no songs by Tai Verdes in this playlist.”
+It is expected that your program will be well documented and must contain a comment block at the beginning that includes the following information in an easy-to-read format:  the file name, your name, the project number, the course identifier (CMSC 256), and the current semester, and a brief description of the file’s purpose.
+*/
+
+
 package cmsc256;
 
 import bridges.base.SLelement;
